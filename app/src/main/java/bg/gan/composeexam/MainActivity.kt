046e72219517d3c640ecd2c81fd9eeb9701bce80
Admin <georgihristovkostov@gmail.com>
2023-03-20 20:30:01 +0200

    adding dependencies and other libraries

--- app/src/main/java/bg/gan/composeexam/MainActivity.kt
+++ app/src/main/java/bg/gan/composeexam/MainActivity.kt
@@ -12,32 +12,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import bg.gan.composeexam.ui.theme.ComposeExamTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeExamTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            ElderAppTheme {
+                ElderApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeExamTheme {
-        Greeting("Android")
     }
 }
